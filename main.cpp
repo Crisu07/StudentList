e40d15c5922bf444f7cdefc9a1885a75d265818d
Chris <42982458+Crisu07@users.noreply.github.com>
2023-08-27 19:58:26 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-//  main.cpp
-// 	Chris Nguyen
-//	CECS 282-04 (Tue/Thur)
-//	Prog 3 – Student Structs
-// 	Due 10/12/2021
-// 
-//  I certify that this program is my own original work. I did not copy any part of this program from 
-//  any other source. I further certify that I typed each and every line of code in this program.
 #include "myDate.h" // added 2 functions to mydate(at the bottom of file): return julian number and print bday
 #include <iostream>
 #include <string>
